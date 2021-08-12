9a67acd172cdcdd6edb91f68681b0d85417160f0
Alexander Sklar <asklar@microsoft.com>
2021-08-11 20:51:42 -0700

    >

--- .github/workflows/scripts/unpackGoodIdls.ps1
+++ .github/workflows/scripts/unpackGoodIdls.ps1
@@ -1,4 +1,12 @@
 
-Write-host "The ZIP ==> " $env:GOOD_IDL_ZIP
+$zip = $env:GOOD_IDL_ZIP
+
+Write-host "The ZIP ==> " $zip
+
+if ($zip -ne $null) {
+  Write-Host "OK"
+} else {
+  Write-Host "ERROR"
+}
 
 $bytes = gc C:\temp\goodIDLs\goodIDLs.zip -Encoding utf8
